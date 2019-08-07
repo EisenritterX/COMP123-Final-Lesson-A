bfd50c3598f130e7b299a68bbfbafd577daa2dcd
George Zhou <georgezhou89@hotmail.com>
2019-08-07 16:52:17 -0400

    Add project files.

---
+++ COMP123-Final-Lesson-A/Program.cs
@@ -0,0 +1,23 @@
+﻿using COMP123_Final_Lesson_A.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Final_Lesson_A
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MasterForm());
+        }
+    }
+}
